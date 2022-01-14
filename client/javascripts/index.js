897b5836d7c1a86c1c8ccc35182fe3b0ce28d483
Sharifdesigns22 <shariftarver@gmail.com>
2022-01-13 21:59:30 -0500

    edit / update button is now functional

--- client/javascripts/index.js
+++ client/javascripts/index.js
@@ -1,4 +1,4 @@
-/*jshint esversion: 8 */
+/*jshint esversion: 9 */
 
 // global variabless
 const baseUrl = 'http://localhost:3000';
@@ -8,21 +8,23 @@ let comments = [];
 
 // node getters
 
-function strategyList() {
-    return document.getElementById('strategies');
-}
-const strategyForm = () => document.getElementById('strategy-form')
-const strategyFormSubmit = () => document.getElementById('submit')
-const getStrategyName = () => document.getElementById('name')
-const getStrategyReference = () => document.getElementById('reference')
-const getStrategyTier = () => document.getElementById('tier')
-const getStrategyCategory = () => document.getElementById('category')
-const getStrategyDescription = () => document.getElementById('description')
+
+const strategyForm = () => document.getElementById('strategy-form');
+const strategyFormSubmit = () => document.getElementById('submit');
+const getStrategyName = () => document.getElementById('name');
+const getStrategyReference = () => document.getElementById('reference');
+const getStrategyTier = () => document.getElementById('tier');
+const getStrategyCategory = () => document.getElementById('category');
+const getStrategyDescription = () => document.getElementById('description');
+const formHeader = () => document.getElementById('form-header');
+const getComTitle = () =>document.getElementById('title');
+const getComBody = () => document.getElementById('body');
+const strategyList = () => document.getElementById('strategies');
 
 
 const resetStrategies = () => {
-    strategyList().innerHTML = ''
-}
+    strategyList().innerHTML = '';
+};
 
 // event listeners
 
@@ -30,7 +32,7 @@ const resetStrategies = () => {
 
 const attachFormEvents = () => {
     strategyForm().addEventListener('submit', createStrategy);
-}
+};
 
 
 //event handler
@@ -38,21 +40,16 @@ const attachFormEvents = () => {
 const createStrategy = async (e) => {
     e.preventDefault();
 
-    const description = document.getElementById('description').value;
-    const reference = document.getElementById('reference').value;
-    const tier = document.getElementById('tier').value;
-    const category = document.getElementById('category').value;
-    const name = document.getElementById('name').value;
 
     const strongParams = {
         strategy: { 
-        name,
-        tier,
-        description,
-        reference,
-        category
+        name: getStrategyName().value,
+        description: getStrategyDescription().value,
+        tier: getStrategyTier().value,
+        reference: getStrategyReference().value,
+        category: getStrategyCategory().value
         }
-    }
+    };
 
     const res = await fetch(baseUrl + "/strategies", {
         method: "POST",
@@ -90,20 +87,56 @@ const deleteStrategy = async strategy => {
     
     };
 
-const editStrategy = async (e) => {
-    const id = e.target.dataset.id;
-    const res = await fetch(baseUrl + `/strategies/${id}`, {
-        method: 'PUT',
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-        },
-    }
-    );
-    const strategy = await res.json();
 
-    alert('Strategy successfully edited');
-    renderStrategy(strategy);
+const editStrategy = strategy => {
+    strategyForm().removeEventListener('submit', createStrategy);
+    getStrategyName().value = strategy.name;
+    getStrategyDescription().value = strategy.description;
+    getStrategyReference().value = strategy.reference;
+    getStrategyTier().value = strategy.tier;
+    getStrategyCategory().value = strategy.category;
+    strategyFormSubmit().value = 'Update Strategy';
+    formHeader().innerText = 'Edit strategy';
+    updateStrategy = updateStrategy.bind(strategy); 
+    strategyForm().addEventListener('submit', updateStrategy);
+ 
+
+    };
+    
+updateStrategy = async e => {
+    e.preventDefault();
+  
+    console.log(this);
+  
+ const strongParams = {
+    strategy: {
+        name: getStrategyName().value,
+        description: getStrategyDescription().value,
+        reference: getStrategyReference().value,
+        tier: getStrategyTier().value,
+        category: getStrategyCategory().value
+        }
+    };
+  
+    const response = await fetch(baseUrl + `/strategies/${ this.id }`, {
+      method: 'PATCH',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(strongParams)
+    });
+  
+    const newStrategy = await response.json();
+    const index = strategies.indexOf(this);
+    strategies[index] = newStrategy;
+  
+    strategyForm().removeEventListener('submit', updateStrategy);
+    formHeader().innerText = "Create Strategy";
+    strategyFormSubmit().value = "Create Strategy";
+    strategyForm().addEventListener('submit', createStrategy);
+  
+    renderStrategy();
 };
 
 
@@ -111,17 +144,16 @@ const createComment = async (c) => {
     c.preventDefault();
 
     const id = c.target.dataset.id;
-    const title = document.getElementById('title').value;
-    const body = document.getElementById('body').value;
+    
     const strongParams = {
         comment: {
-            title,
-            body
+            title: getComTitle().value,
+            body: getComBody().value
         }
-    }
+    };
 
     const res = await fetch(baseUrl + `/comments/${id}`, {
-        method: 'PUT',
+        method: 'POST',
         headers: {
             "Accept": "application/json",
             "Content-Type": "application/json"
@@ -132,7 +164,7 @@ const createComment = async (c) => {
     renderComment(comment);
     alert('Comment successfully added');
 
-}  
+};  
 
 const deleteComment = async (comment) => {
     // fetching the strategy with the ID of the strategy we want to delete.
@@ -143,7 +175,7 @@ const deleteComment = async (comment) => {
     comments = comments.filter(s => s.id !== id);
 
     renderComment(comment);
-}
+};
 
 
 
@@ -152,7 +184,7 @@ const deleteComment = async (comment) => {
 
 // this function creates a new div element and adds it to the strategyList() element
 const renderStrategy = (strategy) => {
-    const strategyList = () => document.getElementById('strategies')
+
     const div = document.createElement('div');
     const h3 = document.createElement('h3');
     const h4 = document.createElement('h4');
@@ -166,46 +198,50 @@ const renderStrategy = (strategy) => {
     // creates two new elements, h3 and p, and adds them to the new div
     // sets the textContent of the h3, p elements to the strategy content.
    
-    h2.innerText = strategy['name'];
-    h4.innerText = strategy['tier'];
-    h3.textContent = strategy['category'];
-    console.log(strategy);
-    p3.innerText = strategy['description'];
-    p.innerText = strategy['reference'];
-    p4.innerText = strategy['created_at'];
-    
-    editButton.innerText = 'Edit'
-    editButton.addEventListener('click', e => editStrategy(strategy))
-    
-    deleteButton.innerText = 'Delete'
-    deleteButton.addEventListener('click', e => deleteStrategy(strategy))
+    h2.innerText = strategy.name;
+    h4.innerText = strategy.tier;
+    h3.textContent = strategy.category;
+    p3.innerText = strategy.description;
+    p.innerText = strategy.reference;
+    p4.innerText = strategy.created_at;
 
-    p.style.color = 'blue'
-    h3.style.color = 'green'
+    // sets the textContent of the editButton and deleteButton to 'Edit' and 'Delete'
+    
+    editButton.innerText = 'Edit';
+    editButton.addEventListener('click', e => editStrategy(strategy));
+    
+    deleteButton.innerText = 'Delete';
+    deleteButton.addEventListener('click', e => deleteStrategy(strategy));
+
+    // appends the new div to the strategyList() element, apply class name strategy card
+
+    p.style.color = 'blue';
+    h3.style.color = 'green';
+    div.className = 'strategy-card';
    
 
     // adding elements into the DOM
-    div.appendChild(h2)
-    div.appendChild(h3)
-    div.appendChild(h4)
-    div.appendChild(p3)
-    div.appendChild(p)
-    div.appendChild(p4)
-    div.appendChild(deleteButton)
-    div.appendChild(editButton)
+    div.appendChild(h2);
+    div.appendChild(h3);
+    div.appendChild(h4);
+    div.appendChild(p3);
+    div.appendChild(p);
+    div.appendChild(p4);
+    div.appendChild(deleteButton);
+    div.appendChild(editButton);
 
-    strategyList().appendChild(div)
+    strategyList().appendChild(div);
     return strategyList();
 
-}
+};
 
 const renderStrategies = async () => {
     // clear the list of any existing strategies
     resetStrategies();
-    strategies = await loadStrategies()
-    // loops through the strategies array and calls the renderStrategy() function for each blog
+    strategies = await loadStrategies();
+    // loops through the strategies array and calls the renderStrategy() function for each strategy
     strategies.forEach(strategy => renderStrategy(strategy));
-}
+};
 
 
 const loadStrategy = async () => {
@@ -229,27 +265,38 @@ const loadComments = async () => {
 
 
 const renderComments = async (comment) => {
-    const comments = await loadComments(strategy);
+    
     const commentList = document.getElementById('comments');
-    comments.forEach(comment => {
-        const div = document.createElement('div');
-        const h3 = document.createElement('h3');
-        const p = document.createElement('p');
-        const deleteButton = document.createElement('button');
-        const editButton = document.createElement('button');
-        }
-    )
+    const div = document.createElement('div');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+    const deleteButton = document.createElement('button');
+    const editButton = document.createElement('button');
+     
 
-    h3.innerText = comment['title'];
-    p.innerText = comment['body'];
-    deleteButton.innerText = 'Delete'
-    deleteButton.addEventListener('click', e => deleteComment(comment))
-    editButton.innerText = 'Edit'
-    editButton.addEventListener('click', e => editComment(comment))
+    h3.innerText = comment.title;
+    p.innerText = comment.body;
 
+    // sets the textContent of the editButton and deleteButton to 'Edit' and 'Delete'
+
+    deleteButton.innerText = 'Delete';
+    deleteButton.addEventListener('click', e => deleteComment(comment));
+
+
+    editButton.innerText = 'Edit';
+    editButton.addEventListener('click', e => editComment(comment));
+
+    // appends the new div to the commentsList() element, apply class name comment card
+    div.className = 'comment-card';
+    div.appendChild(h3);
+    div.appendChild(p);
+    div.appendChild(deleteButton);
+    div.appendChild(editButton);
+
+    
     commentList().appendChild(div);
 
-}
+};
 
 document.addEventListener('DOMContentLoaded', () => {
     loadStrategy();

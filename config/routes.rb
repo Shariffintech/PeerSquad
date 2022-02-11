3b0baca2f5b7407e5a01f12ca5dae759c3aea8e6
Sharifdesigns22 <shariftarver@gmail.com>
2022-02-10 20:49:25 -0500

    railways deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # set the root path
-  root 'strategies#index'
-  
+  # set the root path to index.html
+  root 'application#index'
+
   resources :strategies do
     resources :comments
   end
